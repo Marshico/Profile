da25d434f570fed4ec1fec79b06aefa25365d239
Rez Bst <153891357+Marshalcoder@users.noreply.github.com>
2025-03-31 14:25:55 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['wattshare-images.s3.us-east-1.amazonaws.com'], // Add your image domain here
+  },
+};
+
+module.exports = nextConfig; 
